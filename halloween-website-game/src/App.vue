b61b4f5e868b720853885dcda6282e0e3820c9b4
KingYano <belkadi.yanis.l@gmail.com>
2023-10-22 23:32:04 +0200

    initialisation du projet avec fichier image et sass

---
+++ halloween-website-game/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Hello World</h1>
+  </main>
+</template>
+
+<script setup>
+  console.log('test')
+</script>
+
+<style lang="scss">
+  main {
+    h1 {
+      color: red !important;
+    }
+  }
+
+</style>
