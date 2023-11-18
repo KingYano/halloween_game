9a5c5a09281ac1308a61fd473bdaf656095e4bf5
Yanis <belkadi.yanis.l@gmail.com>
2023-11-18 12:23:48 +0100

    reactivation animation gsap

--- halloween-website-game/src/components/HomeSection.vue
+++ halloween-website-game/src/components/HomeSection.vue
@@ -34,6 +34,7 @@
 <script setup>
 import GameModal from './GameModal.vue';
 import { ref, watch, onMounted } from 'vue';
+import { gsap } from 'gsap';
 
 const showModal = ref(false);
 
@@ -59,15 +60,15 @@ watch(showModal, (newValue) => {
 });
 
 onMounted(() => {
-//   gsap.from('.home__shadow', 1.5, {opacity: 0, y: -300, delay: .2});
-//   gsap.from('.home__points', 1.5, {opacity: 0, y: -300, delay: .4});
-//   gsap.from('.home__grass', 1.5, {opacity: 0, y: 300, delay: .2, ease: 'elastic.out(1, .5)'});
-//   gsap.from('.home__pumpkin', 1.5, {opacity: 0, y: -300, delay: .6, ease: 'bounce.out'});
-//   gsap.from('.home__stones', 1.5, {opacity: 0, y: 300, delay: .8, ease: 'elastic.out(1, .5)'});
-//   gsap.from('.home__moon', 1.5, {opacity: 0, y: 300, delay: 1.5, ease: 'elastic.out(1, .5)'});
-//   gsap.from('.home__titles', 1.5, {opacity: 0, y: -300, delay: 1.5, ease: 'elastic.out(1, .5)'});
-//   gsap.from('.home__trees', 1.5, {opacity: 0, y: 300, delay: 2, ease: 'elastic.out(1, .5)'});
-//   gsap.from('.home__data', 1.5, {opacity: 0, y: 200, delay: 2, ease: 'elastic.out(1, .5)'});
+  gsap.from('.home__shadow', 1.5, {opacity: 0, y: -300, delay: .2});
+  gsap.from('.home__points', 1.5, {opacity: 0, y: -300, delay: .4});
+  gsap.from('.home__grass', 1.5, {opacity: 0, y: 300, delay: .2, ease: 'elastic.out(1, .5)'});
+  gsap.from('.home__pumpkin', 1.5, {opacity: 0, y: -300, delay: .6, ease: 'bounce.out'});
+  gsap.from('.home__stones', 1.5, {opacity: 0, y: 300, delay: .8, ease: 'elastic.out(1, .5)'});
+  gsap.from('.home__moon', 1.5, {opacity: 0, y: 300, delay: 1.5, ease: 'elastic.out(1, .5)'});
+  gsap.from('.home__titles', 1.5, {opacity: 0, y: -300, delay: 1.5, ease: 'elastic.out(1, .5)'});
+  gsap.from('.home__trees', 1.5, {opacity: 0, y: 300, delay: 2, ease: 'elastic.out(1, .5)'});
+  gsap.from('.home__data', 1.5, {opacity: 0, y: 200, delay: 2, ease: 'elastic.out(1, .5)'});
 });
 
 
